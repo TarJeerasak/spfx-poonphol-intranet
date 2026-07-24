80a0f61986a1ab08b02938c5ff57a45f65a0e0db
TarJeerasak <jeerasakso@fusionsoft.co.th>
2026-07-24 14:05:27 +0700

    Initial commit

---
+++ src/webparts/spfxPoonpholIntranet/components/ISpfxPoonpholIntranetProps.ts
@@ -0,0 +1,7 @@
+export interface ISpfxPoonpholIntranetProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/spfxPoonpholIntranet/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISpfxPoonpholIntranetWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SpfxPoonpholIntranetWebPartStrings' {
+  const strings: ISpfxPoonpholIntranetWebPartStrings;
+  export = strings;
+}
